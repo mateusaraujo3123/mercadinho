605acbf4017a32d2f8b0f1ce5472e207104b18e7
mateusaraujo3123 <LEIVISONMATEUS2021@GMAIL.COM>
2026-07-11 19:44:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from streamlit_gsheets import GSheetsConnection
+import gspread
+from google.oauth2.service_account import Credentials
 
 # Configuração estável da página para ocupar a tela toda
 st.set_page_config(
